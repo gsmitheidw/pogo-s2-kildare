cf23ee84a4c8a037aef4358fac8a932caf974dd4
G Smith <gsmitheidw@gmail.com>
2025-06-08 17:09:45 +0100

    s2/news update

--- s2.py
+++ s2.py
@@ -159,24 +159,54 @@ for poi in pois:
     note_text = f"<br><br>💡<strong>Notes:</strong> {note}" if isinstance(note, str) and note.strip() else ""
     popup_text = f"{icon} {poi['name']} ({poi['type']}){note_text}"
 
-    marker = folium.Marker(
-        location=[poi["lat"], poi["lng"]],
-        popup=popup_text,
-        icon=folium.Icon(color=color, icon="info-sign")
-    )
-    group.add_child(marker)
+
+	marker = folium.Marker(
+    	location=[poi["lat"], poi["lng"]],
+	    icon=folium.Icon(color=color, icon="info-sign"),
+	    popup=folium.Popup(f"""
+    	    <b>{icon} {poi['name']} ({poi['type']})</b>{note_text}
+	        <script>
+	            var marker = this;
+	            marker._popup.once('add', function() {{
+    	           var map = marker._map;
+	               var circle = L.circle([{poi["lat"]}, {poi["lng"]}], {{
+	                   radius: 80,
+    	               color: '{color}',
+        	           weight: 1,
+            	       fill: true,
+                	   fillColor: '{color}',
+                    	illOpacity: 0.15
+	               }}).addTo(map);
+    	       }});
+	       </script>
+	   """, max_width=300)
+	)
+	group.add_child(marker)
+
+
+
+
+
+
+
+#    marker = folium.Marker(
+#        location=[poi["lat"], poi["lng"]],
+#        popup=popup_text,
+#        icon=folium.Icon(color=color, icon="info-sign")
+#    )
+#    group.add_child(marker)
 
 
     # Add interaction circle
-    folium.Circle(
-        location=[poi["lat"], poi["lng"]],
-        radius=80,
-        color=color,
-        weight=1,
-        fill=True,
-        fill_color=color,
-        fill_opacity=0.15
-    ).add_to(range_fg)
+    #folium.Circle(
+    #    location=[poi["lat"], poi["lng"]],
+    #    radius=80,
+    #    color=color,
+    #    weight=1,
+    #    fill=True,
+   #     fill_color=color,
+   #     fill_opacity=0.15
+   # ).add_to(range_fg)
 
 
 
@@ -186,7 +216,7 @@ pokestops.add_to(m)
 nominated.add_to(m)
 potential.add_to(m)
 notpogo.add_to(m)
-range_fg.add_to(m) # 80m range per poi
+#range_fg.add_to(m) # 80m range per poi
 
 # Add Esri sat
 folium.TileLayer(
